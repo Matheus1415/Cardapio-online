13409b3c389c72810d574997f81d0c09e18c0a95
Matheus1415 <115950084+Matheus1415@users.noreply.github.com>
2024-03-04 19:58:28 -0300

    desestruturando o array

--- src/pages/Cardapio/Itens/item/index.tsx
+++ src/pages/Cardapio/Itens/item/index.tsx
@@ -5,29 +5,24 @@ import cardapio from './../itens.json'
 type Props = typeof cardapio[0]
 
 export const Item = (props: Props) => {
+
+    const {title, description, category, size, serving, price} = props
+
     return(
         <div className={styles.item}>
             <div className={styles.item__imagem}>
-                <img src={logo} alt="imagens" />
+                <img src={logo} alt={title} />
             </div>
             <div className={styles.item__descricao}>
                 <div className={styles.item__titulo}>
-                    <h2>Macarrão</h2>
-                    <p>Descrição do Macarrão</p>
+                    <h2>{title}</h2>
+                    <p>{description}</p>
                 </div>
                 <div className={styles.item__tags}>
-                    <div className={styles.item__tipo}>
-                        Massa
-                    </div>
-                    <div className={styles.item__procao}>
-                        600g
-                    </div>
-                    <div className={styles.item__qtdpessoas}>
-                        Serve 2 pessoas
-                    </div>
-                    <div className={styles.item__valor}>
-                        R$ 50
-                    </div>
+                    <div className={styles.item__tipo}>{category.label}</div>
+                    <div className={styles.item__procao}>{size}g</div>
+                    <div className={styles.item__qtdpessoas}>Serve {serving} pessoas</div>
+                    <div className={styles.item__valor}>R$ {price.toFixed(2)}</div>
                 </div>
             </div>
         </div>
