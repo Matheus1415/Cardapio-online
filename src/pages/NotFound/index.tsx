e9565aedbbcca3266f29f1f28b083238ce51108d
Matheus1415 <115950084+Matheus1415@users.noreply.github.com>
2024-03-15 10:54:15 -0300

    NotFounf concluido

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,9 +1,22 @@
-import styles from './NoteFound.module.scss'
+import styles from './NoteFound.module.scss';
+import { ReactComponent as NotFoundImagem } from 'assets/not_found.svg';
+import classNames from 'classnames';
+import styleTema from '../../styles/Tema.module.scss';
+import { useNavigate } from 'react-router-dom';
 
-export const NotFounf = () =>{
-    return(
-        <div>
-            
-        </div>
-    )
-}
+export const NotFound = () => {
+  const navigate = useNavigate();
+  return (
+    <div className={classNames({
+      [styles.container]: true,
+      [styleTema.container]: true
+    })}>
+      <div className={styles.voltar}>
+        <button onClick={() => navigate(-1) }>
+          {'< Voltar'}
+        </button>
+      </div>
+      <NotFoundImagem />
+    </div>
+  );
+};
